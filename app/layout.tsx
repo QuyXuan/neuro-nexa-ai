37084fbc97719de182a4d29e1ac93c8015ecb455
QuyXuan <maitrinhxuanquy@gmail.com>
2024-01-15 11:46:41 +0700

    feat: Format code block

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "../node_modules/highlight.js/styles/night-owl.css";
 import SideBar from "@/components/SideBar";
 import { SessionProvider } from "@/components/SessionProvider";
 import { getServerSession } from "next-auth";
--- components/Message.tsx
+++ components/Message.tsx
@@ -1,5 +1,6 @@
 import { DocumentData } from "firebase-admin/firestore";
 import Image from "next/image";
+import CodeBlock from "./CodeBlock";
 
 type Props = {
   message: DocumentData;
@@ -8,9 +9,59 @@ type Props = {
 function Message({ message }: Props) {
   const isChatBotRender = message.user.name === "NeuroNexa";
 
+  const extractCodeBlocks = (text: string) => {
+    console.log(text);
+    const regex = /(?:```([\s\S]*?)```|`([^`]+)`|([^`]+))/g;
+    let match;
+    let result = [];
+
+    while ((match = regex.exec(text)) !== null) {
+      if (match[1] !== undefined) {
+        result.push({ content: match[1].trim(), type: "code" });
+      } else if (match[2] !== undefined) {
+        if (result.length > 0 && result[result.length - 1].type === "text") {
+          result[result.length - 1].content = `<p>${result[
+            result.length - 1
+          ].content.replace(
+            /^<p>|<\/p>$/g,
+            ""
+          )}<code style="font-size: .875em; font-weight: 600; color: #1c93ed;"> ${escapeHtml(
+            match[2].trim()
+          )} </code></p>`;
+        } else {
+          result.push({
+            content: `<p><code style="font-size: .875em; font-weight: 600; color: #1c93ed;"> ${escapeHtml(
+              match[2].trim()
+            )} </code></p>`,
+            type: "text",
+          });
+        }
+      } else if (match[3] !== undefined) {
+        if (result.length > 0 && result[result.length - 1].type === "text") {
+          result[result.length - 1].content = `<p>${result[
+            result.length - 1
+          ].content.replace(/^<p>|<\/p>$/g, "")}${match[3].trim()}</p>`;
+        } else {
+          result.push({
+            content: `<p>${match[3].trim()}</p>`,
+            type: "text",
+          });
+        }
+      }
+    }
+    return result;
+  };
+
+  const escapeHtml = (content: string) => {
+    return content
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+  };
+
   return (
     <div className={`py-5 text-white ${isChatBotRender && "bg-[#434654]"}`}>
-      <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+      <div className="flex space-x-5 px-10 max-w-3xl mx-auto">
         <Image
           src={`${
             isChatBotRender
@@ -22,7 +73,22 @@ function Message({ message }: Props) {
           height="24"
           alt="avatar"
         />
-        <p className="pt-1 text-sm">{message.text}</p>
+        <div className="flex flex-col">
+          {extractCodeBlocks(message.text).map((textCode, index) =>
+            textCode.type === "text" ? (
+              <div
+                key={index}
+                className="mb-2"
+                dangerouslySetInnerHTML={{ __html: textCode.content }}
+              />
+            ) : (
+              <CodeBlock
+                key={`${textCode.type}${index}`}
+                message={textCode.content}
+              />
+            )
+          )}
+        </div>
       </div>
     </div>
   );
